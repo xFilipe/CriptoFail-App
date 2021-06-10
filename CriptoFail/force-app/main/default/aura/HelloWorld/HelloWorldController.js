eca94b4f44c560ab75f908f9e4ed8cad0436b2c2
Filipe Lima <32749271+xFilipe@users.noreply.github.com>
2021-06-10 20:51:43 -0300

    update

--- CriptoFail/force-app/main/default/aura/HelloWorld/HelloWorldController.js
+++ CriptoFail/force-app/main/default/aura/HelloWorld/HelloWorldController.js
@@ -3,3 +3,13 @@
 
     }
 })
+
+
+({
+    handleFocus: function (cmp, event) {
+        console.log('Input Seven has recieved the focus.');
+    },
+    handleBlur: function (cmp, event) {
+        console.log('Input Seven has released the focus.');
+    }
+});
