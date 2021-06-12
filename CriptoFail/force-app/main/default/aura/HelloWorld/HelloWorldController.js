d118d478d7c7748a7126bbb60597256c2ce7a3c5
Filipe Lima <32749271+xFilipe@users.noreply.github.com>
2021-06-12 11:58:58 -0300

    update

--- CriptoFail/force-app/main/default/aura/HelloWorld/HelloWorldController.js
+++ CriptoFail/force-app/main/default/aura/HelloWorld/HelloWorldController.js
@@ -1,4 +1,3 @@
-
 ({
     handleClick : function (component, event, helper) {
         var num1 = component.get("v.nome");         
@@ -8,4 +7,22 @@
         component.set("v.nome", "novo valor mano")
 
     }
+
+
+    
 });
+
+/**
+ * 
+ * Progress Bar
+({
+    onRender: function (cmp) {
+        var interval = setInterval($A.getCallback(function () {
+            var progress = cmp.get('v.progress');
+            cmp.set('v.progress', progress === 100 ? clearInterval(interval) : progress + 10);
+        }), 200);
+    }
+})
+
+
+ */
